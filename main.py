a851ef10553909bc9b147283d6ebc46df5bd7f4f
Jelili Adesina <jelilio.adesina@gmail.com>
2023-05-27 10:48:46 +0100

    play UNDERWORLD

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from utils.buzzer_player import BuzzerPlayer
+from utils.buzzer_type import BuzzerType
 
 
 def destroy():
@@ -8,8 +9,9 @@ def destroy():
 
 if __name__ == "__main__":
     try:
-        buzzer_player = BuzzerPlayer(11)
+        buzzer_player = BuzzerPlayer(buzzer_pin=11)
         buzzer_player.setup()
+        buzzer_player.play(buzzer_type=BuzzerType.UNDERWORLD)
         destroy()
     except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
         destroy()
